2ecf7ddd9072b12909d9830f13248ceda06e9253
AliakseiKrasko <alekskrasko1985@gmail.com>
2024-12-18 20:19:25 +0300

    refactoring testet in file user-reducer.test.ts

--- src/state/user-reducer.tx.ts
+++ src/state/user-reducer.tx.ts
@@ -13,11 +13,13 @@ type ActionType = {
 export const userReducer = (state: StateType, action: ActionType) => {
     switch (action.type) {
         case "INCREMENT-AGE":
-            state.age = state.age + 1;
-            return state;
+            const newState = {...state};
+            newState.age = state.age + 1;
+            return newState;
         case "INCREMENR-CHILDREN-COUNT":
-            state.childrenCount = state.childrenCount + 1;
-            return state;
+            return {
+                ...state, childrenCount: state.childrenCount + 1,
+            }
         default:
             throw new Error("I don't understand action type");
     }
