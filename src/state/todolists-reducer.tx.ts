b6a5ebb1bb8e2f5fd07968832c95392d716ded40
AliakseiKrasko <alekskrasko1985@gmail.com>
2024-12-18 21:20:06 +0300

    added test remove todolistreducer

--- src/state/todolists-reducer.tx.ts
+++ src/state/todolists-reducer.tx.ts
@@ -9,7 +9,8 @@ type ActionType = {
 
 export const todolistsReducer = (state: Array<TodoListsPropsType>, action: ActionType): Array<TodoListsPropsType> => {
     switch (action.type) {
-
+        case 'REMOVE-TODOLIST':
+            return state.filter(t => t.id !== action.id)
         default:
             throw new Error("I don't understand action type");
     }
