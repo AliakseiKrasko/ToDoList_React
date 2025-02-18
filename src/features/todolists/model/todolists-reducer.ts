bdb424a44cd861c728755feb9b1d95abad7e8d62
AliakseiKrasko <alekskrasko1985@gmail.com>
2025-02-18 07:10:54 +0100

    added catch in addtodolisttc

--- src/features/todolists/model/todolists-reducer.ts
+++ src/features/todolists/model/todolists-reducer.ts
@@ -90,15 +90,20 @@ export const fetchTodolistsTC = (dispatch: Dispatch, getState: () => RootState) 
 
 export const addTodolistTC = (title: string) => (dispatch: Dispatch) => {
   dispatch(setAppStatusAC("loading"))
-  todolistsApi.createTodolist(title).then((res) => {
-    if (res.data.resultCode === ResultCode.Success) {
-      dispatch(addTodolistAC(res.data.data.item))
-      dispatch(setAppStatusAC("succeeded"))
-    } else {
-      dispatch(setAppErrorAC(res.data.messages.length ? res.data.messages[0] : "Some error occurred."))
-      dispatch(setAppStatusAC("failed"))
-    }
-  })
+  todolistsApi
+    .createTodolist(title)
+    .then((res) => {
+      if (res.data.resultCode === ResultCode.Success) {
+        dispatch(addTodolistAC(res.data.data.item))
+        dispatch(setAppStatusAC("succeeded"))
+      } else {
+        dispatch(setAppErrorAC(res.data.messages.length ? res.data.messages[0] : "Some error occurred."))
+        dispatch(setAppStatusAC("failed"))
+      }
+    })
+    .catch((err) => {
+      dispatch(setAppErrorAC(err.message))
+    })
 }
 
 export const removeTodolistTC = (id: string) => (dispatch: Dispatch) => {
