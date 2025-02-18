31617b5aa2741431acef1dc25ce09631434bce72
AliakseiKrasko <alekskrasko1985@gmail.com>
2025-02-18 09:07:02 +0100

    added <t> handleapperror.ts

--- src/common/utils/handleAppError.ts
+++ src/common/utils/handleAppError.ts
@@ -1,7 +1,11 @@
 import { setAppErrorAC, setAppStatusAC } from "../../app/app-reducer"
 import { AppDispatch } from "../../app/store"
+import { BaseResponse } from "common/types/types"
 
-export const HandleAppError = (dispatch: AppDispatch, data: any) => {
+// BaseResponse<{ item: DomainTask }>
+//BaseResponse<{ item: Todolist }>
+
+export const HandleAppError = <T>(dispatch: AppDispatch, data: BaseResponse<T>) => {
   dispatch(setAppErrorAC(data.messages.length ? data.messages[0] : "Some error occurred."))
   dispatch(setAppStatusAC("failed"))
 }
