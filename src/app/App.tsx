f0b118c4399794451fb3ce85f04110a6d5f77ac5
AliakseiKrasko <alekskrasko1985@gmail.com>
2025-03-09 17:48:05 +0100

    added useeuery in app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,22 +4,32 @@ import { getTheme } from "common/theme/theme"
 import { Header } from "common/Header/Header"
 import { selectThemeMode } from "./app-selector"
 import { useAppSelector } from "common/hooks/useAppSelector"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { ErrorSnackbar } from "common/components/ErrorSnackbar/ErrorSnackbar"
 import { Routing } from "common/routing"
 import { CircularProgress } from "@mui/material"
 import s from "./App.module.css"
+import { useMeQuery } from "../features/auth/api/authApi"
+import { useAppDispatch } from "common/hooks/useAppDispatch"
+import { ResultCode } from "../features/todolists/lib/enams"
+import { setAppStatus, setIsLoggedIn } from "./appSlice"
 
 function App() {
   const themeMode = useAppSelector(selectThemeMode)
   // const isInitialized = useAppSelector(selectIsInitialized)
-  const [isInitialized, setisInitialized] = useState(true)
+  const [isInitialized, setisInitialized] = useState(false)
+  const dispatch = useAppDispatch()
 
-  // const dispatch = useAppDispatch()
-  //
-  // useEffect(() => {
-  //   dispatch(initializeTC())
-  // }, [])
+  const { data } = useMeQuery()
+
+  useEffect(() => {
+    if (data) {
+      if (data?.resultCode === ResultCode.Success) {
+        dispatch(setIsLoggedIn({ isLoggedIn: true }))
+      }
+      setisInitialized(true)
+    }
+  }, [data])
 
   if (!isInitialized) {
     return (
